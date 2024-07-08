99428a1f3f0a7ea58df9b8cb5cfbfa684798d433
patripedregal <patripedregal@gmail.com>
2024-07-08 16:20:52 +0100

    upload

--- 3_Reporting/server.R
+++ 3_Reporting/server.R
@@ -1790,7 +1790,7 @@ output$incidencePlot_std <- renderPlot(
   get_incidence_plot_std()
 )
 
-output$incidence_download_plot_std <- downloadHandler(
+output$incidence_download_plotstd <- downloadHandler(
   filename = function() {
     "Std_incidence_estimates_plot.png"
   },
